eade81ab3972070850bb5b24c3601bcaafe05c27
John Lobo <john.lobo@hotmail.com>
2017-07-03 02:20:52 +0200

    collision - WIP

--- src/util/util.h
+++ src/util/util.h
@@ -15,6 +15,9 @@ typedef struct{
     cpct_keyID camelotmode;
 } TKeys;
 
+u8 fast_collision(u8 x1, u8 y1, u8 w1, u8 h1, u8 x2, u8 y2, u8 w2, u8 h2);
+void reset_cpc();
+
 char* itoa(int value, char* result, int base);
 void delay(u32 cycles);
 u8 checkKeys(const cpct_keyID* k, u8 numk);
