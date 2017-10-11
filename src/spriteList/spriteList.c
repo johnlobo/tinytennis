0f221b26fe992f3b01a8dc418275663423f19354
john.lobo <john.lobo@hotmail.com>
2017-10-11 12:41:52 -0400

    Sprite List 2  Changes to be committed: 	modified:   src/spriteList/spriteList.c

--- src/spriteList/spriteList.c
+++ src/spriteList/spriteList.c
@@ -16,6 +16,8 @@
 #include <cpctelera.h>
 #include "../defines.h"
 #include "spriteList.h"
+#include "../util/video.h"
+#include "../levels/court01.h"
 
 TSpriteList spriteList;
 
@@ -25,6 +27,7 @@ void initSpriteList(){
 
 void addSprite(TEntity *e){
     u8 i;
+    
     if (spriteList.count<MAX_SPRITE_LIST){
         i = 0;
         while ((i<MAX_SPRITE_LIST) && (spriteList.list[i]->id !=0 )){
@@ -40,6 +43,7 @@ void addSprite(TEntity *e){
 
 void deleteSprite(u8 id){
     u8 i = 0;
+    
     while ((i<MAX_SPRITE_LIST) && (spriteList.list[i]->id != id )){
         i++;
     }
@@ -52,6 +56,7 @@ void deleteSprite(u8 id){
 void orderList(){
     u8 i = 1;  // Begin with one to avoid to overflow the array
     TEntity *aux;
+    
     if (spriteList.count > 1){
         while ((i<spriteList.count) && (spriteList.list[i]->y[0] > spriteList.list[i-1]->y[0])){
             i++;
@@ -64,6 +69,48 @@ void orderList(){
     }
 }
 
+void eraseEntity(TEntity *e)
+{
+    //u8 *pvmem;
+    u8 posx, posy;
+    posx = e->x[1] / SCALE;
+    posy = e->y[1] / SCALE;
+    if (((posx + e->w) <= WIDTH) && ((posy + e->h) <= HEIGHT))
+    {
+        cpct_etm_drawTileBox2x4 (posx / 2, posy / 4, (e->w / 2) + 1, (e->h / 4) + 1, MAP_WIDTH, g_scrbuffers[0], court);
+    }
+
+}
+
+void drawEntity(TEntity *e)
+{
+    u8 *pvmem;
+    u8 posx, posy;
+    posx = e->x[0] / SCALE;
+    posy = e->y[0] / SCALE;
+    pvmem = cpct_getScreenPtr((u8 *) g_scrbuffers[0], posx, posy);
+    cpct_drawSpriteMaskedAlignedTable(e->frame->sprite, pvmem, e->w, e->h, g_tablatrans);
+}
+
+
+void entityUpdate(TEntity *e)
+{
+    e->x[1] = e->x[0];
+    e->y[1] = e->y[0];
+    e->z[1] = e->z[0];
+    e->draw = 0;
+}
+
 void printSprites(){
+    u8 i;
+    
+    for (i=0;i<MAX_SPRITE_LIST;i++){
+        if (spriteList.list[i]->draw){
+            eraseEntity((TEntity*) &spriteList.list[i]);
+            drawEntity((TEntity*) &spriteList.list[i]);
+            entityUpdate((TEntity*) &spriteList.list[i]);
+        }
+    }
+    
     
 }
